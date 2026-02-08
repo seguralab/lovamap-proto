f0f99f0d72ccd668129583277b5b70f791935155
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-02-08 13:26:08 -0800

    Format

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 import subprocess
 
+
 class BuildProto(build_py):
     def run(self):
         # Generate protobuf files before building
@@ -12,15 +13,13 @@ class BuildProto(build_py):
         proto_files = ["schemas/Descriptors.proto"]
 
         for proto_file in proto_files:
-            subprocess.check_call([
-                "protoc",
-                f"--python_out={output_dir}",
-                f"-I{proto_dir}",
-                proto_file
-            ])
+            subprocess.check_call(
+                ["protoc", f"--python_out={output_dir}", f"-I{proto_dir}", proto_file]
+            )
 
         # Continue with normal build
         super().run()
+
 
 setup(
     name="lovamap-proto",
