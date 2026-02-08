b497a0ef146e619fe41937aedc252b2d18071ad0
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-02-08 13:12:08 -0800

    Use `setup.py` to initialize setup

--- tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Lovamap protobuf tools package."""
-
-__version__ = "0.2.0"
-
-from .pb2json import protobuf_to_json
-from .json2pb import json_to_protobuf
-
-__all__ = ["protobuf_to_json", "json_to_protobuf"]
